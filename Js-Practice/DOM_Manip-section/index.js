3f88bdf5e56e88e8c4cf2e4541db304c8755d189
jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2024-02-13 00:02:26 -0600

    Changes in paths of the files

---
+++ Js-Practice/DOM_Manip-section/index.js
@@ -0,0 +1,54 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+
+
+const parRed = document.createElement('p');
+parRed.classList.add('red-text');
+parRed.style.color = "red";
+parRed.textContent = "Hey I'm red!";
+
+container.appendChild(parRed);
+
+
+const blueSubTitle = document.createElement('h3');
+blueSubTitle.classList.add('blue-h3');
+blueSubTitle.style.color = "blue";
+blueSubTitle.textContent = "I'm a blue h3";
+
+container.appendChild(blueSubTitle);
+
+const body = document.querySelector("body")
+
+
+const pinkContainer = document.createElement('div');
+pinkContainer.classList.add("pink-container");
+pinkContainer.style.border = "2px solid black";
+pinkContainer.style.backgroundColor = "pink";
+
+container.appendChild(pinkContainer);
+
+const h1div = document.createElement("h1");
+h1div.classList.add("h1InDiv");
+h1div.textContent = "I'm in a div!";
+
+pinkContainer.appendChild(h1div);
+
+const p2 = document.createElement("p");
+p2.textContent = "ME TOO";
+
+pinkContainer.appendChild(p2);
+
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello Wordl");
+
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener('click', () => {
+    alert("Hello World");
+});
